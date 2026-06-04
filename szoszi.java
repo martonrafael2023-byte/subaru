24441732317bcf23287c843848d6f72bbf8cf2fc
martonrafael2023-byte <marton.rafael.2023@osztalyterem.katolikuskeri.hu>
2026-06-04 10:59:07 +0200

    Create szoszi.java

---
+++ szoszi.java
@@ -0,0 +1,12 @@
+static void shop() {
+
+    System.out.println("\n🛒 SHOP");
+    System.out.println("Gold: " + gold);
+
+    System.out.println("1 Potion (20)");
+    System.out.println("2 +10 HP (30)");
+    System.out.println("3 +5 DMG (40)");
+    System.out.println("4 +5% Crit (60)");
+    System.out.println("5 Exit");
+
+    int c = getNumber();
