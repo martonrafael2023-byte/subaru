5def743169323447c2a3758300c52ccb7a94429a
martonrafael2023-byte <marton.rafael.2023@osztalyterem.katolikuskeri.hu>
2026-06-04 11:14:35 +0200

    Create csa.java

---
+++ csa.java
@@ -0,0 +1,8 @@
+System.out.println("Legyozted: " + e.name);
+
+int reward = 10 + room * 2;
+gold += reward;
+
+        System.out.println("💰 +" + reward + " gold");
+        System.out.println("Összes gold: " + gold);
+    }
