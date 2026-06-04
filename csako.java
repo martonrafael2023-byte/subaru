b29fb706a61e4fdc4d02fb8085eb0446ad7b2946
martonrafael2023-byte <marton.rafael.2023@osztalyterem.katolikuskeri.hu>
2026-06-04 10:59:26 +0200

    Create csako.java

---
+++ csako.java
@@ -0,0 +1,9 @@
+else {
+        System.out.println("Nincs eleg gold vagy exit.");
+        }
+                }
+
+static void chestEvent() {
+
+    System.out.println("\nLada!");
+    System.out.println("1 kinyit 2 nem");
